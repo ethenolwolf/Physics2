0598830a8895eb257ec15ed9498fc2968958b84a
Naved Ahmad <ethenolwolf@gmail.com>
2016-09-17 01:34:12 +0530

    First Commit

---
+++ android/src/com/gfs/physics2/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gfs.physics2;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gfs.physics2.Physics2;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Physics2(), config);
+	}
+}
